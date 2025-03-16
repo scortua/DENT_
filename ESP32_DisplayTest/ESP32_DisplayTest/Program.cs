01874423599c5c67aa2cc1ea1ad9885f78127abe
santiago <scortua@hotmail.com>
2025-03-16 18:28:17 -0500

    ADD UPDATE actualizacion readme y test de pantalla para esp32 s3

---
+++ ESP32_DisplayTest/ESP32_DisplayTest/Program.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Device.Gpio;
+using nanoFramework.Hardware.Esp32;
+using nanoFramework.UI;
+using nanoFramework.UI.GraphicDrivers;
+
+namespace ESP32_DisplayTest
+{
+    public class Program
+    {
+        private const int ChipSelect = 5;
+        private const int DataCommand = 4;
+        private const int Reset = 8;
+        private const int backlight = 15;
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from and generic grahic drivers!");
+            var gpioController = new GpioController();
+
+            GpioPin Bl = gpioController.OpenPin(backlight, PinMode.Output);
+            Bl.Write(PinValue.Low);
+
+            // You **MUST** have a build of nanoFramework with a generic graphic driver
+
+            // If you're using an ESP32, don't forget to set the pins for the screen
+            // Set the pins for the screen
+            Configuration.SetPinFunction(7, DeviceFunction.SPI1_MOSI);  // LCD SDA
+            Configuration.SetPinFunction(6, DeviceFunction.SPI1_CLOCK); // LCD SCL 
+            // This is not used but must be defined
+            Configuration.SetPinFunction(46, DeviceFunction.SPI1_MISO);  // LCD MISO SIN importancia
+
+            var displaySpiConfig = new SpiConfiguration(
+                1,
+                ChipSelect,
+                DataCommand,
+                Reset,
+                -1);
+
+            // Get the generic driver
+            GraphicDriver graphicDriver = St7789.GraphicDriver;
+
+            // As optional, you can adjust anything
+            graphicDriver.OrientationPortrait = new byte[]
+            {
+                (byte)GraphicDriverCommandType.Command, 2, 0x36, 0x88,
+            };
+
+            var screenConfig = new ScreenConfiguration(
+                0,
+                20, // offset inicia con 320
+                240, // vertical resolution
+                280, // horizontal resolution
+                graphicDriver);
+
+            var init = DisplayControl.Initialize(
+                displaySpiConfig,
+                screenConfig,
+            1024);
+
+            Bl.Write(PinValue.High);
+            Debug.WriteLine($"Screen initialized");
+
+            ushort[] toSend = new ushort[100];
+            var blue = Color.Blue.ToBgr565();
+            var red = Color.Red.ToBgr565();
+            var green = Color.Green.ToBgr565();
+            var white = Color.White.ToBgr565();
+
+            for (int i = 0; i < toSend.Length; i++)
+            {
+                toSend[i] = blue;
+            }
+
+            DisplayControl.Write(30, 30, 10, 10, toSend); // x, y, width, height, data
+
+            for (int i = 0; i < toSend.Length; i++)
+            {
+                toSend[i] = red;
+            }
+
+            DisplayControl.Write(220, 100, 10, 10, toSend); // x, y, width, height, data
+
+            for (int i = 0; i < toSend.Length; i++)
+            {
+                toSend[i] = green;
+            }
+
+            DisplayControl.Write(20, 149, 10, 10, toSend); // x, y, width, height, data
+
+            for (int i = 0; i < toSend.Length; i++)
+            {
+                toSend[i] = white;
+            }
+
+            DisplayControl.Write(69, 149, 10, 10, toSend); // x, y, width, height, data
+
+            Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,35 @@
 # .NET Folder Overview
 
+<div align="center">
+
+<a href="https://github.com/nanoframework" style="background: linear-gradient(to right, #4B0082, #800080); color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Nanoframework GitHub</a> | <a href="https://nanoframework.net/" style="background: linear-gradient(to right, #006400, #228B22); color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Nanoframework Web</a>
+
+</div>
 This folder contains all the necessary files and projects related to the .NET framework, specifically focusing on the usage of the .NET nanoFramework. Below is a detailed explanation of the contents and their purposes:
 
 ## .NET nanoFramework
 
 The .NET nanoFramework is a free and open-source platform that enables the writing of managed code applications for constrained embedded devices. It is a streamlined version of the .NET framework, designed to run on microcontrollers and other resource-constrained devices.
+
+In [nanoframework firmware](https://github.com/nanoframework/nanoFirmwareFlasher) you can see how do you can flash your devices, in my case is esp32 rev3.
+<details>
+<summary>Download windows to esp32</summary>
+
+> dotnet tool install -g nanoff
+
+> dotnet tool update -g nanoff
+
+> nanoff --listports
+
+> nanoff --platform esp32 --serialport COMXX --devicedetails 
+
+> nanoff --update --target ESP32 --serialport COMXX
+
+> nanoff --platform esp32 --serialport COMXX --update 
+
+</details>
+
+In [nanoframework samples](https://github.com/nanoframework/Samples) you can see the different examples code.
 
 ### Key Features
 
@@ -35,6 +60,30 @@ The .NET nanoFramework is a free and open-source platform that enables the writi
 - **Blinking LED**: A simple project to blink an LED on a microcontroller.
 - **Sensor Data Logger**: Collects data from various sensors and logs it for analysis.
 - **Home Automation**: Controls home appliances using a microcontroller and .NET nanoFramework.
+
+### GPIO Usage
+
+The .NET nanoFramework provides extensive support for General-Purpose Input/Output (GPIO) pins, allowing you to interact with various hardware components.
+
+- **Reading from GPIO**: Read the state of a GPIO pin to detect input signals.
+- **Writing to GPIO**: Set the state of a GPIO pin to control output devices like LEDs or relays.
+- **Interrupts**: Configure GPIO pins to trigger interrupts on state changes, useful for handling events like button presses.
+
+### Wi-Fi Usage
+
+The .NET nanoFramework includes libraries for managing Wi-Fi connections, enabling your microcontroller to connect to wireless networks.
+
+- **Connecting to Wi-Fi**: Use the Wi-Fi API to connect to a network by specifying the SSID and password.
+- **Network Communication**: Implement network communication protocols like HTTP or MQTT to interact with web services or IoT platforms.
+- **Wi-Fi Events**: Handle events related to Wi-Fi connectivity, such as connection status changes or network errors.
+
+### Multithreading
+
+The .NET nanoFramework supports multithreading, allowing you to run multiple tasks concurrently.
+
+- **Creating Threads**: Create and manage threads to perform background tasks without blocking the main application.
+- **Thread Synchronization**: Use synchronization primitives like mutexes and semaphores to coordinate access to shared resources.
+- **Thread Safety**: Ensure thread-safe operations to prevent race conditions and data corruption.
 
 ### Additional Resources
 
